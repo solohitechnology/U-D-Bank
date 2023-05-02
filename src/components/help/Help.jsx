e540b75c921ecaad2e779440d8bac4eb4f1b136d
John Uguru <Johnuguru73@gmail.com>
2023-05-02 15:11:08 +0100

    Minor changes

--- src/components/help/Help.jsx
+++ src/components/help/Help.jsx
@@ -8,27 +8,20 @@ export const Help = () => {
             <div className="help">
                 <div className="help_content">
                     <h1>We are here to help</h1>
-
                     <p className='chat'>Chat with us via our in-app live chat or send us massage on our official social media pages. if you inquire further assisteance, contact us via the following channels </p>
-
                     <div className="contact">
                         <p>
                             <label><a href='#' >09123456789</a> OR <a href='#'>091-2345</a></label>
                             <label><a href='#' >09123456789</a> OR <a href='#'>091-2345</a></label>
 
-                            <label><a href='#'>costomersercvice@U~Dbank-inc.com</a></label>
+                            <label><a href='#'>customerservice@U~Dbank-inc.com</a></label>
                         </p>
                     </div>
 
                 </div>
-
-
                 <div className="img_container">
                     <img src='help.jpg' alt='img' />
                 </div>
-
-
-
             </div>
         </>
     )
--- src/components/mobile/Mobile.jsx
+++ src/components/mobile/Mobile.jsx
@@ -4,8 +4,8 @@ import { Scroll } from '../scrollmove/Scroll';
 export const Mobile = () => {
 
     const img = [
-     {id: 1, link: "/solo", src: "mobileapp3.jpg", hd: "Bank Transfer",pg: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo omnis error, dicta facilis itaque tempore  ."},
-     {id: 2, link: "/tech", src: "mobileapp2.png", hd: "Flexible Saving",pg: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo omnis error, dicta facilis itaque tempore  ."}   
+        { id: 1, link: "/solo", src: "mobileapp3.jpg", hd: "Bank Transfer", pg: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo omnis error, dicta facilis itaque tempore  ." },
+        { id: 2, link: "/tech", src: "mobileapp2.png", hd: "Flexible Saving", pg: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo omnis error, dicta facilis itaque tempore  ." }
     ]
 
     // const Scroll = () => {
@@ -14,27 +14,25 @@ export const Mobile = () => {
 
     return (
         <>
-                   <div className='wrapper'> 
+            <div className='wrapper'>
+                <div className='mobileapps'>
+                    {img.map((item) => {
+                        return (
+                            <a href={item.link}>
+                                <div className='imgraper'>
+                                    <img src={item.src} key={item.id} alt='solo_img' />
+                                    <Scroll>
+                                        <h3>{item.hd}</h3>
+                                        <legend>{item.pg}</legend>
+                                    </Scroll>
 
-            <div className='mobileapps'>
+                                </div>
+                            </a>
+                        )
+                    })}
 
-                {img.map((item) => {
-                    return (
-                        <a href={item.link}>
-                        <div  className='imgraper'>
-                            <img src={item.src} key={item.id} alt='solo_img'/>
-                            <Scroll>
-                            <h3>{item.hd}</h3>
-                         <legend> {item.pg}</legend>
-                            </Scroll>
-
-                        </div>
-                        </a>
-                    )
-                })}
-
+                </div>
             </div>
-                   </div>
 
         </>
     )
